c701b5897df1ac5d50d97e3cd2d755a8156bff75
David Ebbo <david.ebbo@gmail.com>
2017-03-08 07:38:23 -0800

    Init

---
+++ CronTester/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NCrontab;
+
+namespace CronTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Pass cron expression on command line");
+                return;
+            }
+
+            try
+            {
+                var schedule = CrontabSchedule.Parse(args[0], new CrontabSchedule.ParseOptions { IncludingSeconds = true });
+
+                int count = 0;
+                foreach (var occurence in schedule.GetNextOccurrences(DateTime.Now, DateTime.Now.AddYears(1)))
+                {
+                    Console.WriteLine(occurence);
+
+                    count++;
+                    if (count == 10) break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
